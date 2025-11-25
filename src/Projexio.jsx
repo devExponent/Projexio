3e8aa9b988f1d79644229190b2b7bff0ab7e0688
Okunlola Muhammad Toyeeb <96062255+devExponent@users.noreply.github.com>
2025-11-25 11:57:01 +0200

    Progress

--- src/Projexio.jsx
+++ src/Projexio.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from "react";
 import Projects from "./components/Projects";
 import Tasks from "./components/Tasks";
+import Modal from "./components/Modal";
 
 function Projexio() {
   const [addProject, setAddProject] = useState(false);
@@ -100,112 +101,125 @@ function Projexio() {
   };
 
   return (
-    <div className="flex bg-gray-100 h-screen">
-      <aside className="w-100 bg-black text-white p-5 shadow-md hidden md:block rounded-tr-2xl ">
-        <h1 className="text-2xl font-bold my-6">YOUR PROJECTS</h1>
+    <>
+      {!isValid && (
+        <Modal onClose={() => setIsValid(true)}>
+          <p>Wrong Input</p>
+        </Modal>
+      )}
 
-        <div>
-          <button
-            className="bg-stone-600 rounded-xl py-3 px-6 text-lg text-center text-white"
-            onClick={AddProject}
-          >
-            + Add Project
-          </button>
-        </div>
-        {saveTask.length <= 0 ? (
-          <p className="my-3">You have no project yet!</p>
-        ) : (
-          <>
-            {saveTask.map((project, index) => (
-              <div
-                key={index}
-                className="cursor-pointer hover:bg-stone-600  rounded-xl my-3 p-2"
-                onClick={() => {
-                  setSelectedProjectIndex(index);
-                  setAddProject(false);
-                }}
-              >
-                {project.title}
-              </div>
-            ))}
-          </>
-        )}
-      </aside>
+      <div className="flex bg-gray-100 h-screen">
+        <aside className="w-100 bg-black text-white p-5 shadow-md hidden md:block rounded-tr-2xl ">
+          <h1 className="text-2xl font-bold my-6">YOUR PROJECTS</h1>
 
-      {/* Main Content */}
-      <main className="flex-1 p-6 overflow-auto">
-        {/* Header */}
-        <header className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-semibold">Welcome, Exponent</h2>
-          <div className="flex items-center gap-4">
-            <button className="p-2 bg-gray-200 rounded hover:bg-gray-300">
-              Notifications
+          <div>
+            <button
+              className="bg-stone-600 rounded-xl py-3 px-6 text-lg text-center text-white"
+              onClick={AddProject}
+            >
+              + Add Project
             </button>
-            <div className="p-2 bg-gray-200 rounded">Profile</div>
           </div>
-        </header>
-
-        <div className="grid place-items-center h-full">
-          <div className="w-full md:w-10/12 mx-auto">
-            {addProject ? (
-              <Projects
-                projects={projects}
-                onChange={taskInput}
-                onSave={save}
-                saveTask={saveTask}
-                onCancel={onCancel}
-                titleRef={titleRef}
-                dateRef={datRef}
-                descriptionRef={descriptionRef}
-                validation={isValid}
-              />
-            ) : selectedProjectIndex !== null ? (
-              <div className=" p-6 rounded shadow-md">
-                <div className="my-5">
-                  <div className="flex justify-between items-center">
-                    <h2 className="text-4xl font-bold text-stone-600 mb-4">
-                      {saveTask[selectedProjectIndex].title}
-                    </h2>
-                    <button className="hover:text-red-500" onClick={deletePro}>
-                      Delete
-                    </button>
-                  </div>
-                  <p className="mb-2 text-stone-400 text-2xl">
-                    {saveTask[selectedProjectIndex].date}
-                  </p>
-                  <p className="mb-2">
-                    {saveTask[selectedProjectIndex].description}
-                  </p>
-                </div>
-
-                <div>
-                  <Tasks
-                    onClick={addTasks}
-                    handleTasks={saveTask[selectedProjectIndex].tasks}
-                    projectTasks={projectTasks}
-                    setProjectTasks={setProjectTasks}
-                    clearTask={clearTask}
-                  />
-                </div>
-              </div>
-            ) : (
-              <div className="text-center">
-                <h1 className="font-bold text-3xl my-8">No Project Selected</h1>
-                <p className="text-xl my-8">
-                  Select a project or get started with a new one
-                </p>
-                <button
-                  className="bg-stone-600 rounded-xl py-3 px-6 text-lg text-center text-white"
-                  onClick={AddProject}
+          {saveTask.length <= 0 ? (
+            <p className="my-3">You have no project yet!</p>
+          ) : (
+            <>
+              {saveTask.map((project, index) => (
+                <div
+                  key={index}
+                  className="cursor-pointer hover:bg-stone-600  rounded-xl my-3 p-2"
+                  onClick={() => {
+                    setSelectedProjectIndex(index);
+                    setAddProject(false);
+                  }}
                 >
-                  Create a new Project
-                </button>
-              </div>
-            )}
+                  {project.title}
+                </div>
+              ))}
+            </>
+          )}
+        </aside>
+
+        {/* Main Content */}
+        <main className="flex-1 p-6 overflow-auto">
+          {/* Header */}
+          <header className="flex justify-between items-center mb-6">
+            <h2 className="text-2xl font-semibold">Welcome, Exponent</h2>
+            <div className="flex items-center gap-4">
+              <button className="p-2 bg-gray-200 rounded hover:bg-gray-300">
+                Notifications
+              </button>
+              <div className="p-2 bg-gray-200 rounded">Profile</div>
+            </div>
+          </header>
+
+          <div className="grid place-items-center h-full">
+            <div className="w-full md:w-10/12 mx-auto">
+              {addProject ? (
+                <Projects
+                  projects={projects}
+                  onChange={taskInput}
+                  onSave={save}
+                  saveTask={saveTask}
+                  onCancel={onCancel}
+                  titleRef={titleRef}
+                  dateRef={datRef}
+                  descriptionRef={descriptionRef}
+                  validation={isValid}
+                />
+              ) : selectedProjectIndex !== null ? (
+                <div className=" p-6 rounded shadow-md">
+                  <div className="my-5">
+                    <div className="flex justify-between items-center">
+                      <h2 className="text-4xl font-bold text-stone-600 mb-4">
+                        {saveTask[selectedProjectIndex].title}
+                      </h2>
+                      <button
+                        className="hover:text-red-500"
+                        onClick={deletePro}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                    <p className="mb-2 text-stone-400 text-2xl">
+                      {saveTask[selectedProjectIndex].date}
+                    </p>
+                    <p className="mb-2">
+                      {saveTask[selectedProjectIndex].description}
+                    </p>
+                  </div>
+
+                  <div>
+                    <Tasks
+                      onClick={addTasks}
+                      handleTasks={saveTask[selectedProjectIndex].tasks}
+                      projectTasks={projectTasks}
+                      setProjectTasks={setProjectTasks}
+                      clearTask={clearTask}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <div className="text-center">
+                  <h1 className="font-bold text-3xl my-8">
+                    No Project Selected
+                  </h1>
+                  <p className="text-xl my-8">
+                    Select a project or get started with a new one
+                  </p>
+                  <button
+                    className="bg-stone-600 rounded-xl py-3 px-6 text-lg text-center text-white"
+                    onClick={AddProject}
+                  >
+                    Create a new Project
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,19 @@
 import { createPortal } from "react-dom";
+import { useRef, useEffect } from "react";
 
-const Modal = ({ children }) => {
+const Modal = ({ children, onClose }) => {
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    dialogRef.current.showModal(); // REQUIRED
+  }, []);
+
   return createPortal(
-    <dialog>
+    <dialog ref={dialogRef}>
       {children}
+
       <form method="dialog">
-        <button>close</button>
+        <button onClick={onClose}>Close</button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
